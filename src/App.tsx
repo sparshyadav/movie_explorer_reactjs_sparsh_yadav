70e0cf6452d4b2262ed929ec92924da24bd923c2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-24 22:05:45 +0530

    [Sparsh] Add: App.tsx Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Login from './pages/Login/Login'
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <Login />
       </div>
     )
   }
