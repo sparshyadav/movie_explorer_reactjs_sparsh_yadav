1610f57e155d532ac54267a6ef7455554801c8de
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-24 23:22:48 +0530

    [Sparsh] Add: Mobile Field Added in Signup Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup/Signup'
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        app
-      </div>
-    )
-  }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
