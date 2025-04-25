663295f6ec8ba29bb92142246fbf5509d4523f94
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-25 14:46:23 +0530

    [Sparsh] Refactor: Toaster Implemeted in the Application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <Router>
+      <Toaster/> 
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
