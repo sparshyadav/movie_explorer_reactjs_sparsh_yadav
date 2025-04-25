78d3398e8363c7bee412979dec6583ca3160cc6c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-25 16:13:37 +0530

    [Sparsh] Add: Route for Navbar Created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Navbar />} />
       </Routes>
     </Router>
   )
