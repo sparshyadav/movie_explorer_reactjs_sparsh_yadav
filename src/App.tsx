d1477fe952dfc2bbf0736ff73b70493cbaeb150e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-26 15:28:59 +0530

    [Sparsh] Add: Wrapping with UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar/Navbar';
+import { UserProvider } from './context/UserContext';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Router>
-      <Toaster/> 
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Toaster />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/' element={<Navbar />} />
+          <Route path='/footer' element={<Footer />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   )
 }
 
