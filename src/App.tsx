d742ac04a5e7915a871338eb5e8664e12a4465a8
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-26 15:39:08 +0530

    [Sparsh] Add: App Updated with User Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Navbar/Navbar';
 import { UserProvider } from './context/UserContext';
-import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
@@ -14,8 +13,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/' element={<Navbar />} />
-          <Route path='/footer' element={<Footer />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </Router>
     </UserProvider>
