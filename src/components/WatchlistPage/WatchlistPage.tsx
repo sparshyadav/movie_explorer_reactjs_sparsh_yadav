0cd6afd56782b7fc8df9a6aad3994ca873158b83
Sparsh Yadav <sparsh.yadav@magicsw.com>
2025-05-27 13:03:28 +0530

    [Sparsh] Add: Watchlist Page Created

--- src/components/WatchlistPage/WatchlistPage.tsx
+++ src/components/WatchlistPage/WatchlistPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import './WatchlistPage.scss';
-import { getMoviesByGenre, getWatchlist } from '../../API';
+import { getWatchlist } from '../../API';
 import MovieCard from '../MovieCard/MovieCard';
 import CircularProgress from '@mui/material/CircularProgress';
 
@@ -15,76 +15,65 @@ interface Movie {
 }
 
 const WatchlistPage
-: React.FC = () => {
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [activeCategory, setActiveCategory] = useState('All Movies');
-    const [loading, setLoading] = useState(true);
+    : React.FC = () => {
+        const [movies, setMovies] = useState<Movie[]>([]);
+        const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                setLoading(true);
-                let response;
-
-                if (activeCategory === 'All Movies') {
+        useEffect(() => {
+            const fetchMovies = async () => {
+                try {
+                    setLoading(true);
+                    let response;
                     response = await getWatchlist();
-                    console.log("RESPONSE: ", response);
                     setMovies(response);
-                } else {
-                    response = await getMoviesByGenre(activeCategory);
-                    setMovies(response.movies);
+
+                } catch (error) {
+                    console.error(`Error fetching watchlisted movies`, error);
+                } finally {
+                    setLoading(false);
                 }
-            } catch (error) {
-                console.error(`Error fetching watchlisted movies`, error);
-            } finally {
-                setLoading(false);
-            }
-        };
+            };
 
-        fetchMovies();
-    }, [activeCategory]);
+            fetchMovies();
+        }, []);
 
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    }, [activeCategory]);
+        useEffect(() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        }, []);
 
-    const handleGenreChange = async (genre: string) => {
-        setActiveCategory(genre);
-    }
+        return (
+            <Box className="movie-page">
+                <Typography variant="h3" className="page-title">
+                    Your Watchlisted Movies
+                </Typography>
 
-    return (
-        <Box className="movie-page">
-            <Typography variant="h3" className="page-title">
-                Your Watchlisted Movies
-            </Typography>
-
-            <Box className="movies-container">
-                {loading ? (
-                    <Box className="loading">
-                        <Box sx={{ display: 'flex' }}>
-                            <CircularProgress />
+                <Box className="movies-container">
+                    {loading ? (
+                        <Box className="loading">
+                            <Box sx={{ display: 'flex' }}>
+                                <CircularProgress />
+                            </Box>
                         </Box>
-                    </Box>
-                ) : movies.length === 0 ? (
-                    <Box className="no-movies">
-                        <Typography variant="h5">No Movies Found</Typography>
-                    </Box>
-                ) : (
-                    movies.map((movie) => (
-                        <MovieCard
-                            key={movie.id}
-                            premium={movie.premium}
-                            id={movie.id}
-                            title={movie.title}
-                            posterImage={movie.poster_url}
-                            rating={movie.rating}
-                        />
-                    ))
-                )}
+                    ) : movies.length === 0 ? (
+                        <Box className="no-movies">
+                            <Typography variant="h5">No Movies Found</Typography>
+                        </Box>
+                    ) : (
+                        movies.map((movie) => (
+                            <MovieCard
+                                key={movie.id}
+                                premium={movie.premium}
+                                id={movie.id}
+                                title={movie.title}
+                                posterImage={movie.poster_url}
+                                rating={movie.rating}
+                            />
+                        ))
+                    )}
+                </Box>
             </Box>
-        </Box>
-    );
-};
+        );
+    };
 
 export default WatchlistPage;
